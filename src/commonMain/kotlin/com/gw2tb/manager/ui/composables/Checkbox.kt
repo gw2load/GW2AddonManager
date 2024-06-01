1bb6ad6935d948b09e0cf6fe390a9b052b0c7b77
Leon Linhart <themrmilchmann@gmail.com>
2024-06-01 18:26:25 +0200

    feat: add custom Checkbox composable

---
+++ src/commonMain/kotlin/com/gw2tb/manager/ui/composables/Checkbox.kt
@@ -0,0 +1,120 @@
+package com.gw2tb.manager.ui.composables
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsHoveredAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.PointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
+import java.awt.Cursor
+
+/*
+ * This is a modified version of the material Checkbox composable to better fit the design of the app.
+ */
+
+@Composable
+fun Checkbox(
+    checked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    color: Color = Color(0xFF8ad3d3)
+) {
+    val checkCache = remember { CheckDrawingCache() }
+    val isHovered by interactionSource.collectIsHoveredAsState()
+
+    @Suppress("NAME_SHADOWING")
+    val color = if (isHovered) {
+        color
+    } else {
+        lerp(color, Color.Black, 0.4F)
+    }
+
+    Box(
+        modifier = modifier
+            .clip(CircleShape)
+            .pointerHoverIcon(icon = PointerIcon(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)))
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                role = Role.Checkbox,
+                onClick = onClick
+            )
+            .padding(4.dp)
+            .size(20.dp)
+            .padding(3.dp)
+            .border(width = 1.dp, color = color)
+            .drawBehind {
+                if (checked) {
+                    drawCheck(
+                        checkColor = color,
+                        checkFraction = 1F,
+                        crossCenterGravitation = 0f,
+                        strokeWidthPx = 2f,
+                        drawingCache = checkCache
+                    )
+                }
+            }
+    )
+
+}
+
+@Immutable
+private class CheckDrawingCache(
+    val checkPath: Path = Path(),
+    val pathMeasure: PathMeasure = PathMeasure(),
+    val pathToDraw: Path = Path()
+)
+
+private fun DrawScope.drawCheck(
+    checkColor: Color,
+    checkFraction: Float,
+    crossCenterGravitation: Float,
+    strokeWidthPx: Float,
+    drawingCache: CheckDrawingCache
+) {
+    val stroke = Stroke(width = strokeWidthPx, cap = StrokeCap.Square)
+    val width = size.width
+    val checkCrossX = 0.4f
+    val checkCrossY = 0.7f
+    val leftX = 0.2f
+    val leftY = 0.5f
+    val rightX = 0.8f
+    val rightY = 0.3f
+
+    val gravitatedCrossX = androidx.compose.ui.util.lerp(checkCrossX, 0.5f, crossCenterGravitation)
+    val gravitatedCrossY = androidx.compose.ui.util.lerp(checkCrossY, 0.5f, crossCenterGravitation)
+    // gravitate only Y for end to achieve center line
+    val gravitatedLeftY = androidx.compose.ui.util.lerp(leftY, 0.5f, crossCenterGravitation)
+    val gravitatedRightY = androidx.compose.ui.util.lerp(rightY, 0.5f, crossCenterGravitation)
+
+    with(drawingCache) {
+        checkPath.reset()
+        checkPath.moveTo(width * leftX, width * gravitatedLeftY)
+        checkPath.lineTo(width * gravitatedCrossX, width * gravitatedCrossY)
+        checkPath.lineTo(width * rightX, width * gravitatedRightY)
+        // TODO: replace with proper declarative non-android alternative when ready (b/158188351)
+        pathMeasure.setPath(checkPath, false)
+        pathToDraw.reset()
+        pathMeasure.getSegment(
+            0f, pathMeasure.length * checkFraction, pathToDraw, true
+        )
+    }
+    drawPath(drawingCache.pathToDraw, checkColor, style = stroke)
+}
