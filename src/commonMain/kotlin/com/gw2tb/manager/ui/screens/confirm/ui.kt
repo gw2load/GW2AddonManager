bc5f483c790857bdad6dfe18be86d3e2818195d6
Leon Linhart <themrmilchmann@gmail.com>
2025-11-02 20:16:03 +0100

    chore: cleanup

--- src/commonMain/kotlin/com/gw2tb/manager/ui/screens/confirm/ui.kt
+++ src/commonMain/kotlin/com/gw2tb/manager/ui/screens/confirm/ui.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
